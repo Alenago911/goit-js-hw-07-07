d20e96ff14331ced56f9936edf066cc56040b51f
Alenago911 <alena.chernyavka@gmail.com>
2023-03-31 15:57:45 +0300

    gallery 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
 console.log(galleryItems);
+
+function cards (images) {
+return images
+.map((image)=> {
+return `
+<li class="gallery__item">
+<a class="gallery__link" href="${image.original}">
+<img
+class="gallery__image"
+src="${image.preview}"
+data-source="${image.original}"
+alt= "${image.description}"
+/>
+</a>
+</li>`;
+}).join('');
+};
+
+cards(galleryItems);
+const elementUl = document.querySelector("ul");
+elementUl.insertAdjacentHTML('beforeend', cards(galleryItems));
+
+elementUl.addEventListener ('click', onGallery);
+
+function onGallery (evt) {
+evt.preventDefault ();
+if (evt.target.nodeName !== "IMG") {
+return
+}
+const instance = basicLightbox.create (
+`<img src = "${evt.target.dataset.source}">`
+)
+instance.show()
+}
+
+
+
+
+
